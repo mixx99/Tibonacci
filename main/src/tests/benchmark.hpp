96941b18ca896b46780241b61fcc939f1b4edf58
mixx99 <maxkriv1@gmail.com>
2025-06-22 13:03:09 +0300

    fix benchmark bugs

--- main/src/tests/benchmark.hpp
+++ main/src/tests/benchmark.hpp
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "simple-bench.h"
 #include <Tibonacci.hpp>
 
 // std
@@ -8,6 +7,7 @@
 #include <ctime>
 #include <iostream>
 #include <iomanip>
+#include <chrono>
 
 namespace Tests {
 void fill_tib(TIB::Tibonacci &tib, int size_to_fill) {
@@ -36,27 +36,28 @@ void benchmark(int number_of_elements = 10000, int tests = 10,
   double without_threads_result = 0;
 
   TIB::Tibonacci tib;
-  timespec t1, t2;
-  double t;
-  simple_gettime(&t1);
+
+  double diff = 0;
+
   for (int i = 0; i < tests; ++i) {
     fill_tib(tib, number_of_elements);
+    auto start = std::chrono::high_resolution_clock::now();
     tib.calculate_fibonacci_numbers_without_threads();
+    auto end = std::chrono::high_resolution_clock::now();
+    diff = std::chrono::duration<double>(end - start).count();
+    without_threads_result += diff;
   }
-  simple_gettime(&t2);
-  t = diff(t1, t2);
-  without_threads_result = t;
-  std::cout << "Calculating without threads took: " << t << " seconds."
+  std::cout << "Calculating without threads took: " << without_threads_result << " seconds."
             << std::endl;
-  simple_gettime(&t1);
   for (int i = 0; i < tests; ++i) {
     fill_tib(tib, number_of_elements);
+    auto start = std::chrono::high_resolution_clock::now();
     tib.calculate_fibonacci_numbers_with_threads(number_of_threads);
+    auto end = std::chrono::high_resolution_clock::now();
+    diff = std::chrono::duration<double>(end - start).count();
+    with_threads_result += diff;
   }
-  simple_gettime(&t2);
-  t = diff(t1, t2);
-  with_threads_result = t;
-  std::cout << "Calculating with " << number_of_threads << " threads took " << t
+  std::cout << "Calculating with " << number_of_threads << " threads took: " << with_threads_result
             << " seconds. " << std::endl;
   std::cout << "Difference is "
             << std::abs(with_threads_result - without_threads_result)
